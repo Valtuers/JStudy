8a494f99cc5fb32fbfa231ddc7d2289264e9b6f4
Valtuers <1581510541@qq.com>
2020-03-31 14:17:25 +0800

    补充Collection模块

--- src/main/java/com/Collection/ListDemo.java
+++ src/main/java/com/Collection/ListDemo.java
@@ -1,18 +1,30 @@
 package com.Collection;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import com.google.common.collect.Lists;
+
+import java.util.*;
 
 public class ListDemo {
     /*
-      比较ArrayList和LinkedList
+      比较1、ArrayList和2、LinkedList
       获取指定元素： 1、速度很快，2、需要从头开始查找元素
       添加元素到末尾： 1、速度很快，2、速度很快
       在指定位置添加/删除： 1、需要移动元素，2、不需要移动元素
       内存占用： 1、少，2、较大
      */
-
+    public static void main(String[] args) {
+        String[] arr = {"哈哈哈","啦啦啦","哦哦哦"};
+        List<String> a = new ArrayList<>(Arrays.asList(arr));
+        ArrayList<String> newArr = Lists.newArrayList(arr);
+        Collections.reverse(newArr);
+        newArr.add("噗噗噗");
+        for(String s : newArr){
+            System.out.println(s);
+            if("哦哦哦".equals(s)){
+                newArr.remove(s);
+            }
+        }
+    }
 }
 
 class Person{
--- src/main/java/com/Collection/MapDemo.java
+++ src/main/java/com/Collection/MapDemo.java
@@ -1,4 +1,13 @@
 package com.Collection;
 
 public class MapDemo {
+    /*
+        比较1、HashMap和2、HashSet
+        HashSet的底层就是由HashMap实现的
+        线程非安全，要保证线程安全的话可使用ConcurrentHashMap
+        HashMap默认的初始大小为16，之后每次扩充，容量都为原来的2倍
+     */
+    public static void main(String[] args) {
+
+    }
 }
